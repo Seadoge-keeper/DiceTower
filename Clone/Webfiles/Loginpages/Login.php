fc92009d004d1be6faf6ff4c42f4ce38b5975d0c
Sjinga Awal <71959074+Seadoge-keeper@users.noreply.github.com>
2023-06-10 05:03:39 +0400

    stashed changes

--- Clone/Webfiles/Loginpages/Login.php
+++ Clone/Webfiles/Loginpages/Login.php
@@ -1,9 +1,15 @@
+<?php
+session_start();
+
+?>
+
 <head>
     <link rel="stylesheet" href="Login.css">
 </head>
 
 <body>
     <div class="Wrapper">
+<<<<<<< Updated upstream
         <button onclick="history.back()" class="back"> <img src="../../Assets/images/back.png" alt=""> </button>
     <div class="PageContainer">
         <div class="Title">
@@ -13,9 +19,28 @@
         <form action="" class="details">
             <input type="text" placeholder="Username">
             <input type="password" placeholder="Password">
+=======
+        <button onclick="history.back()" class="back"> Back </button>
+        <div class="PageContainer">
+            <div>
+                <img src="../../Assets/images/DiceImg.png" class="icon">
+                <h1>Dice Tower</h1>
+            </div>
+            <form action="Loggingin.php" class="details" method="post">
+                <input type="text" placeholder="Username" name="Username">
+                <input type="password" placeholder="Password" name="Password">
+>>>>>>> Stashed changes
 
-            <br>
+                <?php
+                if (empty($_SESSION['Status'])) {
+                    echo "<br>";
+                } else {
+                    echo $_SESSION['Status'];
+                    $_SESSION['Status'] = null;
+                }
+                ?>
 
+<<<<<<< Updated upstream
             <input type="submit" class="submit">
         </form>
         <div>
@@ -25,5 +50,16 @@
             Forgot password? <a href="./ResetPass.php"> Click Here </a>
         </div>
     </div>
+=======
+                <input type="submit">
+            </form>
+            <div>
+                don't have an account yet? <a href="Registration.php">Click Here</a>
+            </div>
+            <div>
+                Forgot password? <a href="./ResetPass.php">Click Here</a>
+            </div>
+        </div>
+>>>>>>> Stashed changes
     </div>
 </body>
--- Clone/Webfiles/Loginpages/ResetPass.php
+++ Clone/Webfiles/Loginpages/ResetPass.php
@@ -1,25 +1,45 @@
+<?php
+session_start();
+
+?>
+
 <head>
     <link rel="stylesheet" href="Login.css">
 </head>
 
 <body>
     <div class="Wrapper">
+<<<<<<< Updated upstream
     <button onclick="history.back()" class="back"> <img src="../../Assets/images/back.png" alt=""> </button>
     <div class="PageContainer">
         <div>
             <img src="../../Assets/images/Raggy/ResetPass.png" class="icon">
             <h1>Reset Password</h1>
+=======
+        <button onclick="history.back()" class="back"> Back </button>
+        <div class="PageContainer">
+            <div>
+                <img src="../../Assets/images/Raggy/ResetPass.png" class="icon">
+                <h1>Reset Password</h1>
+            </div>
+            <form action="" class="details">
+                <input type="text" placeholder="Username">
+                <input type="text" placeholder="Email">
+                <input type="text" placeholder="Password">
+                <input type="text" placeholder="New Password">
+
+                <?php
+                if (empty($_SESSION['Status'])) {
+                    echo "<br>";
+                } else {
+                    echo $_SESSION['Status'];
+                    $_SESSION['Status'] = null;
+                }
+                ?>
+
+                <input type="submit">
+            </form>
+>>>>>>> Stashed changes
         </div>
-        <form action="" class="details">
-            <input type="text" placeholder="Username">
-            <input type="text" placeholder="Email">
-            <input type="text" placeholder="Password">
-            <input type="text" placeholder="New Password">
-
-            <br>
-
-            <input type="submit">
-        </form>
-    </div>
     </div>
 </body>
